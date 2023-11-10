ca53514044c1225e1272983f6fef2b1c007611ac
Hoang Luong <hoangre2002>
2023-11-10 16:46:22 +0700

    up

---
+++ WMS/WMS/WMS/Controllers/CriteriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WMS.Controllers
+{
+	public class CriteriaController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
